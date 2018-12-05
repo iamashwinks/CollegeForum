26097e16e5613edb60443f62999a424a9e755594
Ashwin <ashwin.k.sri@gmail.com>
2018-12-05 15:19:03 +0530

    Added static root

--- collegeforum/settings.py
+++ collegeforum/settings.py
@@ -123,8 +123,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static"),
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
--- collegeforum/urls.py
+++ collegeforum/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('', home),
     path('post/<int:post_id>', post_page),
     path('blogcomment/<int:post_id>/', blogcomment),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
      urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
